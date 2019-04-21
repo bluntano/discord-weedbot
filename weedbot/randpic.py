85364fc6ba4a9a75ba7ef7a437f18b53fabf99e7
That Silly Blunt! <42609088+thatsillyweed@users.noreply.github.com>
2019-04-22 01:18:37 +0300

    added 3 pics more to the list

--- weedbot/randpic.py
+++ weedbot/randpic.py
@@ -3,7 +3,7 @@ import os
 from random import choice
 from scipy import *
 
-picture_set = ['1.jpg', '2.jpg', '3.jpg', '4.jpg']
+picture_set = ['./bb/1.jpg', './bb/2.jpg', './bb/3.jpg', './bb/4.jpg', './bb/5.png', './bb/6.png', './bb/7.jpg']
 
 def randomPic():
     a = random.choice(picture_set, replace=True)
