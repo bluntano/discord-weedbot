82a6e573701310da5a244b0227fdc8540bab909f
That Silly Blunt! <42609088+thatsillyweed@users.noreply.github.com>
2019-04-24 22:37:01 +0300

    updated the picture list (new system coming ASAP)

--- weedbot/randpic.py
+++ weedbot/randpic.py
@@ -6,6 +6,8 @@ from random import choice
 from scipy import *
 
 # List of pictures in the 'bb' directory
+# God, I need to make a better system for this random choice!
+
 picture_set = [
     './bb/1.jpg',
     './bb/2.jpg',
@@ -16,7 +18,11 @@ picture_set = [
     './bb/7.jpg',
     './bb/8.jpg',
     './bb/9.jpg',
-    './bb/10.png'
+    './bb/10.png',
+    './bb/11.jpg',
+    './bb/12.jpg',
+    './bb/13.png',
+    './bb/14.jpg'
     ]
 
 # Function where it takes a one random choice from the list
