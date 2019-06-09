dee1748293cba9adb7ba9bd52eb4d4f4016a0725
bluntano <42609088+bluntano@users.noreply.github.com>
2019-06-09 03:52:59 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@
 # It also has a pip command that is required to execute before launching the bot.
 # Otherwise the bot won't start up.
 
-# Display Python code logs (Errors, printings, etc.)
-export PYTHONUNBUFFERED=true
-
 # Make sure you have Python 3.5 installed (e.g. 3.5.2)
 pythonversion=$(python3 --version)
-vernum="3.5.2"
+echo $pythonversion
 
-if [[ $pythonversion =~ $vernum ]];
+if [[ $pythonversion =~ "3.5.2" ]];
 then
+  # Display Python code logs (Errors, printings, etc.)
+  export PYTHONUNBUFFERED=true
+  
 	python3 -m pip install --upgrade pip --user
 
 	# Install the requirements
@@ -23,5 +23,5 @@ then
 	# Finally, start the bot
 	python3 WeedBot.py
 else
-	echo "Failed to run :("
+	echo "Failed to run :( Either, Python is not installed or, it is incorrect version."
 fi
