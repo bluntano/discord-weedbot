f63fa9470da0b86f87ad94dc79ddbbfdc083b14a
Bluntano <42609088+bluntano@users.noreply.github.com>
2019-06-11 22:52:30 +0300

    Update DropboxFileUploader.py

--- weedbot/DropboxFileUploader.py
+++ weedbot/DropboxFileUploader.py
@@ -68,8 +68,10 @@ def upload_picture_to_dropbox(url):
         print("Making directory for new received pictures")
         os.mkdir('./upload-pictures/')
 
-    # Download picture from Discord attachments server
+	
 	upload_path = './upload-pictures/' + str(x) + extension
+
+    # Download picture from Discord attachments server
     headers = {
     'User-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
     }
