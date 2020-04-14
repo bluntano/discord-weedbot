b7b43d71de2e5e69a8e79bda06eb0647e9a1a7e6
bluntano <42609088+bluntano@users.noreply.github.com>
2020-04-15 00:04:36 +0300

    Add git POST route

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect
+import subprocess
 import WeedBot
 
 app = Flask(__name__)
@@ -9,6 +10,11 @@ def index():
 @app.route('/bot')
 def bot_invite():
     return redirect(f"https://discordapp.com/api/oauth2/authorize?client_id={WeedBot.bot_id()}&permissions=2064&scope=bot")
+
+@app.route('/git' methods=['POST'])
+def git_update():
+    subprocess.call(['./git.sh'])
+    return "git"
 
 if __name__ == "__main__":
     WeedBot.bot_alive()
