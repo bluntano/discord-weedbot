6ec603a573d4c829c0db48aa0c7d22ec5d28f0bb
bluntano <42609088+bluntano@users.noreply.github.com>
2020-04-15 00:01:09 +0300

    virtualenv didn't work

--- start.sh
+++ start.sh
@@ -9,16 +9,5 @@ export PYTHONUNBUFFERED=true
 # portable MediaInfo library for Linux (otherwise is taken from the system library folder instead)
 export LD_LIBRARY_PATH=~/mediainfo/
 
-# Install Python 3 virtual env
-VIRTUALENV=.data/venv
-
-if ! command -v virtualenv; then
-  pip3 install virtualenv
-fi
-
-if [ ! -d $VIRTUALENV ]; then
-  virtualenv -p /usr/bin/python3 $VIRTUALENV
-fi
-
-$VIRTUALENV/bin/pip3 install -r requirements.txt
-$VIRTUALENV/bin/python3 WeedBot.py
+pip3 install -r requirements.txt
+python3 Server.py
