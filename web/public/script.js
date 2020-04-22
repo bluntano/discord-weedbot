20554800411dbf0c66042bd3738bd5ea3f6e53d2
Bluntano <42609088+bluntano@users.noreply.github.com>
2020-04-22 03:01:30 +0300

    No need for post function

--- web/public/script.js
+++ web/public/script.js
@@ -2,7 +2,6 @@ function checkSubmit() {
     var textline = document.getElementById('textline').value;
     var sets = document.getElementById('sets').value;
     var status = document.querySelector('.submitStatus');
-    //var finalButton = document.querySelector('.finalBtn')
     if (!textline) {
         status.innerHTML = "<p>Please insert text to bot to say!</p>";
     } else if (!textline.includes('{}%')) {
@@ -12,25 +11,4 @@ function checkSubmit() {
     } else {
         status.innerHTML = "<button type=\"submit\">Submit!</button>";
     };
-};
-
-function post(path, params=' ', method='post') {
-    const containerElem = document.querySelector('.container');
-    containerElem.classList.add('animated', 'bounceOutLeft');
-    setTimeout(function() {
-        const form = document.createElement('form');
-        form.method = method;
-        form.action = path;
-        for (const key in params) {
-            if (params.hasOwnProperty(key)) {
-                const hiddenField = document.createElement('input');
-                hiddenField.type = 'hidden';
-                hiddenField.name = key;
-                hiddenField.value = params[key];
-                form.appendChild(hiddenField);
-            };
-        };
-        document.body.appendChild(form);
-        form.submit();
-    }, 1000);
 };
