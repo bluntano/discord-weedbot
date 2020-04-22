3db44e22abffc905c7a3318aaf6768461bf483d3
Bluntano <42609088+bluntano@users.noreply.github.com>
2020-04-22 03:02:51 +0300

    Update Server.py
    - Added new route for Electron app to POST request
    - Removed POST method from submit
    - Removed /index, /invite

--- Server.py
+++ Server.py
@@ -12,26 +12,39 @@ def status_handler(input: str):
     return render_template('header.html') + f"<h1>{input}</h1>" + """
     <script>
         window.onload = setTimeout(function() {
-            window.location.replace('/index');
+            window.location.replace('/');
         }, 3000);
     </script>
     """ + render_template('footer.html')
 
+def process_textline(textline, chosen_set):
+    print(f"{textline}\n{chosen_set}")
+    if not textline or not chosen_set:
+        return status_handler("Why are we still here to suffer?")
+    if len(textline) > 56:
+        return status_handler("Yooo brooo, chill!")
+    with open('sentences.json', 'r') as infile:
+        data = json.load(infile)
+        if textline in data[chosen_set]:
+            return status_handler("It's already there, someone made the joke before you.")
+        data[chosen_set].append(textline)
+        with open('sentences.json', 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+    return status_handler("Yooooo! Poggers, fam!")
+
 app = Flask(__name__, static_folder='web/public', template_folder='web/views')
 @app.route('/')
-@app.route('/index')
 def index():
     return page_handler('index.html')
 
 @app.route('/bot')
-@app.route('/invite', methods=['POST'])
 def bot_invite():
     bot_id = 0
     if not server_debug:
         bot_id = WeedBot.bot_id()
     return redirect(f"https://discordapp.com/api/oauth2/authorize?client_id={bot_id}&permissions=43024&scope=bot")
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit')
 def submit_page():
     return page_handler('submit.html')
 
@@ -45,18 +58,14 @@ def git_update():
 def handle_data():
     textline = request.form['textline']
     chosen_set = request.form['sets']
-    if not textline or not chosen_set:
-        return status_handler("Why are we still here to suffer?")
-    if len(textline) > 56:
-        return status_handler("Yooo brooo, chill!")
-    with open('sentences.json', 'r') as infile:
-        data = json.load(infile)
-        if textline in data[chosen_set]:
-            return status_handler("It's already there, someone made the joke before you.")
-        data[chosen_set].append(textline)
-        with open('sentences.json', 'w') as outfile:
-            json.dump(data, outfile, indent=4)
-    return status_handler("Yooooo! Poggers, fam!")
+    return process_textline(textline, chosen_set)
+
+@app.route('/electronjs/api/textline', methods=['POST'])
+def handle_data_json():
+    data = request.get_json(force=True)
+    textline = data['textline']
+    chosen_set = data['sets']
+    return process_textline(textline, chosen_set)
 
 if __name__ == "__main__":
     if not server_debug:
